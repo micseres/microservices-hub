f3705648ae1fc965e52f3e48b0c22753613a78e6
zogxray <zogxray@gmail.com>
2018-09-06 17:36:30 +0300

    master add test client and service in docker

---
+++ test/service/bin/service.php
@@ -0,0 +1,15 @@
+<?php
+
+$client = new swoole_client(SWOOLE_SOCK_TCP);
+
+if (!$client->connect('10.5.0.111', 9502, -1)) {
+    exit("connect failed. Error: {$client->errCode}\n");
+}
+
+while (true) {
+    $client->send('{"protocol":"1.0","action":"register","route":"sleep","message":"Register me, I am ready","payload":{"route":"sleep","load":"90","time":"09-07-2018 10:00:00.111111"}}');
+    echo $client->recv();
+    sleep(30);
+}
+
+$client->close();
